d5d1c05fd6b59ee914247b657e34576a26d605a5
danjasuw <djaschob@uw.edu>
2018-04-20 10:52:09 -0700

    Add ProxlGenXMLInternalException, needed by last commit

---
+++ src/org/yeastrc/proxl/proxl_gen_import_xml_kojak/common/exceptions/ProxlGenXMLInternalException.java
@@ -0,0 +1,35 @@
+package org.yeastrc.proxl.proxl_gen_import_xml_kojak.common.exceptions;
+
+public class ProxlGenXMLInternalException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ProxlGenXMLInternalException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ProxlGenXMLInternalException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ProxlGenXMLInternalException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ProxlGenXMLInternalException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ProxlGenXMLInternalException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
